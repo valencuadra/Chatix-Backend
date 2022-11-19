f3b360a7a577e0c165c5a5fe7e444ff6f5498c50
Valentin Cuadrado <cuadradosoriano@gmail.com>
2022-11-19 11:04:41 -0300

    pepe

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = require("http").createServer(app);
 const PORT = 5001;
 const io = require("socket.io")(server, {
     cors: {
-        origin: "https://chatix.netlify.app",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 })
@@ -97,5 +97,5 @@ app.get("/rooms", (req, res) => {
 })
 
 server.listen(PORT, ()=> {
-    console.log("Server activiño en el puerto", PORT)
+    console.log("Server activo en el puertiño", PORT)
 })
